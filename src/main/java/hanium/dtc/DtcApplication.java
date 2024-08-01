a830672232c24092abf1a5a861d1f3a56e68227c
Jang99u <alswkdrb@dgu.ac.kr>
2024-08-01 18:49:48 +0900

    feat: first commit

---
+++ src/main/java/hanium/dtc/DtcApplication.java
@@ -0,0 +1,13 @@
+package hanium.dtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanium/dtc/DtcApplicationTests.java
@@ -0,0 +1,13 @@
+package hanium.dtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
