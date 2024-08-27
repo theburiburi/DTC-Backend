8d43f830a328f05a66783e8ffc594687403b3b78
Jang99u <alswkdrb@dgu.ac.kr>
2024-08-27 14:56:13 +0900

    feat: PostRepository 수정

--- src/main/java/hanium/dtc/community/repository/PostRepository.java
+++ src/main/java/hanium/dtc/community/repository/PostRepository.java
@@ -2,6 +2,7 @@ package hanium.dtc.community.repository;
 
 import java.util.Optional;
 import hanium.dtc.community.domain.Post;
+import hanium.dtc.travel.domain.TravelRecord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByIsTravel(boolean isTravel, Pageable pageable);
     Page<Post> findByPostLikeGreaterThanEqual(int likeThreshold, Pageable pageable);
-    Optional<Post> findByTravelId(Long travelId);
+    Optional<Post> findByTravelRecord(TravelRecord travelRecord);
 }
