0d06b4b2dd0460582c4acbb4b971dd549cbba16e
dragonbok <pjs0625@dgu.ac.kr>
2024-08-29 04:58:59 +0900

    좋아요 기능 수정

--- src/main/java/hanium/dtc/community/controller/LikeController.java
+++ src/main/java/hanium/dtc/community/controller/LikeController.java
@@ -1,10 +1,8 @@
 package hanium.dtc.community.controller;
 
-import hanium.dtc.annotation.UserId;
 import hanium.dtc.community.dto.response.LikeResponse;
 import hanium.dtc.global.ResponseDto;
 import hanium.dtc.community.service.LikeService;
-import hanium.dtc.user.domain.User;
 import hanium.dtc.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,24 +16,29 @@ public class LikeController {
     private final UserRepository userRepository;
 
     @PostMapping("/community/{postId}/like")
-    public ResponseDto<LikeResponse> togglePostLike(@UserId Long userId, @PathVariable Long postId) {
-        boolean isLiked = likeService.togglePostLike(postId, userId);
+    public ResponseDto<LikeResponse> togglePostLike(@PathVariable Long postId) {
+
+        boolean isLiked = likeService.togglePostLike(postId);
         int like = likeService.getPostLike(postId);
+
         LikeResponse response = LikeResponse.builder()
                 .isLiked(isLiked)
                 .like(like)
                 .build();
+
         return ResponseDto.ok(response);
     }
 
     @PostMapping("/comments/{postId}/{commentId}/like")
-    public ResponseDto<LikeResponse> toggleCommentLike(@UserId Long userId, @PathVariable Long commentId) {
-        boolean isLiked = likeService.toggleCommentLike(commentId, userId);
+    public ResponseDto<LikeResponse> toggleCommentLike(@PathVariable Long postId, @PathVariable Long commentId) {
+        boolean isLiked = likeService.toggleCommentLike(commentId);
         int like = likeService.getCommentLike(commentId);
+
         LikeResponse response = LikeResponse.builder()
                 .isLiked(isLiked)
                 .like(like)
                 .build();
+
         return ResponseDto.ok(response);
     }
 }
