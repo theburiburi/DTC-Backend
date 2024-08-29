7d776254c65b0bbc08c7a6383d2f9c9a1c3e0cdc
jouijae <kuda1390@dgu.ac.kr>
2024-08-29 16:37:50 +0900

    fix: like 및 commentId 오류 수정

--- src/main/java/hanium/dtc/user/dto/Response/UserCommentResponse.java
+++ src/main/java/hanium/dtc/user/dto/Response/UserCommentResponse.java
@@ -1,7 +1,9 @@
 package hanium.dtc.user.dto.Response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public record UserCommentResponse (
         String nickname,
